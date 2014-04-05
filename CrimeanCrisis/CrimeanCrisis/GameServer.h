2d08fac72ef43a3ce72ee1806426821a3234296a
Felek <tomguz@o2.pl>
2014-04-05 15:06:54 +0200

    szkic projektu, sln

---
+++ CrimeanCrisis/CrimeanCrisis/GameServer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class GameServer
+{
+public:
+	GameServer();
+	~GameServer();
+};
+
---
+++ CrimeanCrisis/CrimeanCrisis/main.cpp
@@ -0,0 +1,8 @@
+#include "Glowny_kontroler.h"
+
+int main(int argc, char const *argv[])
+{
+	Glowny_kontroler kontroler;
+	kontroler.Start();		//tu jest główna pętla gry
+	return 0;
+}

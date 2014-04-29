dd8f4ffba606b69c06a73a8a7576a4b6343c9a5e
Felek <tomguz@o2.pl>
2014-04-30 00:30:43 +0200

    zmiana organizacji folderow

--- przenies wszystko do debuga.bat
+++ przenies wszystko do debuga.bat
@@ -2,14 +2,7 @@ copy bin\*.* CrimeanCrisis\Debug
 copy bin\*.* CrimeanCrisis\CrimeanCrisis\Debug
 
 mkdir CrimeanCrisis\Debug\models
-copy models\*.* CrimeanCrisis\Debug\models
-
-mkdir CrimeanCrisis\CrimeanCrisis\models
-copy models\*.* CrimeanCrisis\CrimeanCrisis\models
-
+copy CrimeanCrisis\CrimeanCrisis\models\*.* CrimeanCrisis\Debug\models
 
 mkdir CrimeanCrisis\Debug\grafiki
-copy grafiki\*.* CrimeanCrisis\Debug\grafiki
-
-mkdir CrimeanCrisis\CrimeanCrisis\grafiki
-copy grafiki\*.* CrimeanCrisis\CrimeanCrisis\grafiki
+copy CrimeanCrisis\CrimeanCrisis\grafiki\*.* CrimeanCrisis\Debug\grafiki
--- przenies wszystko do releasa.bat
+++ przenies wszystko do releasa.bat
@@ -2,13 +2,7 @@ copy bin\*.* CrimeanCrisis\Release
 copy bin\*.* CrimeanCrisis\CrimeanCrisis\Release
 
 mkdir CrimeanCrisis\Release\models
-copy models\*.* CrimeanCrisis\Release\models
-
-mkdir CrimeanCrisis\CrimeanCrisis\Release\models
-copy models\*.* CrimeanCrisis\CrimeanCrisis\models
+copy CrimeanCrisis\CrimeanCrisis\models\*.* CrimeanCrisis\Release\models
 
 mkdir CrimeanCrisis\Release\grafiki
-copy grafiki\*.* CrimeanCrisis\Debug\grafiki
-
-mkdir CrimeanCrisis\CrimeanCrisis\grafiki
-copy grafiki\*.* CrimeanCrisis\CrimeanCrisis\grafiki
+copy CrimeanCrisis\CrimeanCrisis\grafiki\*.* CrimeanCrisis\Debug\grafiki

6bfd1ebbd02bd41756444f33622ab54086a38398
Felek <tomguz@o2.pl>
2014-08-25 17:16:17 +0200

    wprawki do komunikacji uzytecznej

--- CrimeanCrisis/CrimeanCrisis/LogicObject.h
+++ CrimeanCrisis/CrimeanCrisis/LogicObject.h
@@ -5,5 +5,10 @@ class LogicObject
 public:
 	LogicObject();
 	virtual ~LogicObject();
+
+private:
+	int id;
+	int x, y;		// po³o¿enie na mapie
+	int dx, dy;		// po³o¿enie docelowe (jeœli jednostka w ruchu, jeœli stoi to dx == x, dy == y)
 };
 
